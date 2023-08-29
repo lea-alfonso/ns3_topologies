6fec3bd9b1880c81c240eaf0ec3b5f8928e6f09e
Leandro <leandroalfonsoseveri@gmail.com>
2023-08-29 19:18:05 -0300

    lucas topology

---
+++ topologia_1o.cc
@@ -0,0 +1,244 @@
+/* -*-  Mode: C++; c-file-style: "gnu"; indent-tabs-mode:nil; -*- */
+
+// Copyright (c) 2019 Centre Tecnologic de Telecomunicacions de Catalunya (CTTC)
+//
+// SPDX-License-Identifier: GPL-2.0-only
+
+/**
+ * \file cttc-3gpp-channel-example.cc
+ * \ingroup examples
+ * \brief Channel Example
+ *
+ * This example describes how to setup a simulation using the 3GPP channel model
+ * from TR 38.900. Topology consists by default of 2 UEs and 2 gNbs, and can be
+ * configured to be either mobile or static scenario.
+ *
+ * The output of this example are default NR trace files that can be found in
+ * the root ns-3 project folder.
+ */
+
+#include "ns3/applications-module.h"
+#include "ns3/config-store.h"
+#include "ns3/core-module.h"
+#include "ns3/internet-module.h"
+#include "ns3/ipv4-global-routing-helper.h"
+#include "ns3/log.h"
+#include "ns3/mobility-module.h"
+#include "ns3/network-module.h"
+#include "ns3/nr-helper.h"
+#include "ns3/nr-mac-scheduler-tdma-rr.h"
+#include "ns3/nr-module.h"
+#include "ns3/nr-point-to-point-epc-helper.h"
+#include "ns3/point-to-point-helper.h"
+#include <ns3/antenna-module.h>
+#include <ns3/buildings-helper.h>
+
+using namespace ns3;
+
+int
+main(int argc, char* argv[])
+{
+    std::string scenario = "UMa"; // scenario
+    double frequency = 28e9;      // central frequency
+    double bandwidth = 100e6;     // bandwidth
+    double mobility = false;      // whether to enable mobility
+    double simTime = 1;           // in second
+    double speed = 1;             // in m/s for walking UT.
+    bool logging = true; // whether to enable logging from the simulation, another option is by
+                         // exporting the NS_LOG environment variable
+    double hBS;          // base station antenna height in meters
+    double hUT;          // user antenna height in meters
+    double txPower = 40; // txPower
+    enum BandwidthPartInfo::Scenario scenarioEnum = BandwidthPartInfo::UMa;
+
+
+
+    // enable logging
+    if (logging)
+    {
+        // LogComponentEnable ("ThreeGppSpectrumPropagationLossModel", LOG_LEVEL_ALL);
+        // LogComponentEnable("ThreeGppPropagationLossModel", LOG_LEVEL_ALL);
+        // LogComponentEnable ("ThreeGppChannelModel", LOG_LEVEL_ALL);
+        // LogComponentEnable ("ChannelConditionModel", LOG_LEVEL_ALL);
+        // LogComponentEnable ("UdpClient", LOG_LEVEL_INFO);
+        // LogComponentEnable ("UdpServer", LOG_LEVEL_INFO);
+        // LogComponentEnable ("LteRlcUm", LOG_LEVEL_LOGIC);
+        // LogComponentEnable ("LtePdcp", LOG_LEVEL_INFO);
+    }
+
+    /*
+     * Default values for the simulation. We are progressively removing all
+     * the instances of SetDefault, but we need it for legacy code (LTE)
+     */
+    Config::SetDefault("ns3::LteRlcUm::MaxTxBufferSize", UintegerValue(999999999));
+
+    // set mobile device and base station antenna heights in meters, according to the chosen
+    // scenario
+
+
+    // create base stations and mobile terminals
+    NodeContainer enbNodes;
+    NodeContainer ueNodes;
+    enbNodes.Create(2);
+    ueNodes.Create(2);
+
+    // position the base stations
+    Ptr<ListPositionAllocator> enbPositionAlloc = CreateObject<ListPositionAllocator>();
+    enbPositionAlloc->Add(Vector(0.0, 0.0, hBS));
+    enbPositionAlloc->Add(Vector(0.0, 80.0, hBS));
+    MobilityHelper enbmobility;
+    enbmobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    enbmobility.SetPositionAllocator(enbPositionAlloc);
+    enbmobility.Install(enbNodes);
+
+    // position the mobile terminals and enable the mobility
+    Ptr<ListPositionAllocator> uePositionAlloc = CreateObject<ListPositionAllocator>();
+    uePositionAlloc->Add(Vector(3.0, 20, 2));
+    uePositionAlloc->Add(Vector(16.0, 50, 1));
+    MobilityHelper uemobility;
+    uemobility.SetMobilityModel("ns3::ConstantPositionMobilityModel");
+    uemobility.SetPositionAllocator(uePositionAlloc);
+    uemobility.Install(ueNodes);
+
+    /*
+     * Create NR simulation helpers
+     */
+    Ptr<NrPointToPointEpcHelper> epcHelper = CreateObject<NrPointToPointEpcHelper>();
+    Ptr<IdealBeamformingHelper> idealBeamformingHelper = CreateObject<IdealBeamformingHelper>();
+    Ptr<NrHelper> nrHelper = CreateObject<NrHelper>();
+    nrHelper->SetBeamformingHelper(idealBeamformingHelper);
+    nrHelper->SetEpcHelper(epcHelper);
+
+    /*
+     * Spectrum configuration. We create a single operational band and configure the scenario.
+     */
+    BandwidthPartInfoPtrVector allBwps;
+    CcBwpCreator ccBwpCreator;
+    const uint8_t numCcPerBand = 1; // in this example we have a single band, and that band is
+                                    // composed of a single component carrier
+
+    /* Create the configuration for the CcBwpHelper. SimpleOperationBandConf creates
+     * a single BWP per CC and a single BWP in CC.
+     *
+     * Hence, the configured spectrum is:
+     *
+     * |---------------Band---------------|
+     * |---------------CC-----------------|
+     * |---------------BWP----------------|
+     */
+    CcBwpCreator::SimpleOperationBandConf bandConf(frequency,
+                                                   bandwidth,
+                                                   numCcPerBand,
+                                                   scenarioEnum);
+    OperationBandInfo band = ccBwpCreator.CreateOperationBandContiguousCc(bandConf);
+    // Initialize channel and pathloss, plus other things inside band.
+    nrHelper->InitializeOperationBand(&band);
+    allBwps = CcBwpCreator::GetAllBwps({band});
+
+    // Configure ideal beamforming method
+    idealBeamformingHelper->SetAttribute("BeamformingMethod",
+                                         TypeIdValue(DirectPathBeamforming::GetTypeId()));
+
+    // Configure scheduler
+    nrHelper->SetSchedulerTypeId(NrMacSchedulerTdmaRR::GetTypeId());
+
+    // install nr net devices
+    NetDeviceContainer enbNetDev = nrHelper->InstallGnbDevice(enbNodes, allBwps);
+    NetDeviceContainer ueNetDev = nrHelper->InstallUeDevice(ueNodes, allBwps);
+
+    int64_t randomStream = 1;
+    randomStream += nrHelper->AssignStreams(enbNetDev, randomStream);
+    randomStream += nrHelper->AssignStreams(ueNetDev, randomStream);
+
+    // When all the configuration is done, explicitly call UpdateConfig ()
+    for (auto it = enbNetDev.Begin(); it != enbNetDev.End(); ++it)
+    {
+        DynamicCast<NrGnbNetDevice>(*it)->UpdateConfig();
+    }
+
+    for (auto it = ueNetDev.Begin(); it != ueNetDev.End(); ++it)
+    {
+        DynamicCast<NrUeNetDevice>(*it)->UpdateConfig();
+    }
+
+    // create the internet and install the IP stack on the UEs
+    // get SGW/PGW and create a single RemoteHost
+    Ptr<Node> pgw = epcHelper->GetPgwNode();
+    NodeContainer remoteHostContainer;
+    remoteHostContainer.Create(1);
+    Ptr<Node> remoteHost = remoteHostContainer.Get(0);
+    InternetStackHelper internet;
+    internet.Install(remoteHostContainer);
+
+    // connect a remoteHost to pgw. Setup routing too
+    PointToPointHelper p2ph;
+    p2ph.SetDeviceAttribute("DataRate", DataRateValue(DataRate("100Gb/s")));
+    p2ph.SetDeviceAttribute("Mtu", UintegerValue(2500));
+    p2ph.SetChannelAttribute("Delay", TimeValue(Seconds(0.010)));
+    NetDeviceContainer internetDevices = p2ph.Install(pgw, remoteHost);
+
+    Ipv4AddressHelper ipv4h;
+    ipv4h.SetBase("1.0.0.0", "255.0.0.0");
+    Ipv4InterfaceContainer internetIpIfaces = ipv4h.Assign(internetDevices);
+    Ipv4StaticRoutingHelper ipv4RoutingHelper;
+
+    Ptr<Ipv4StaticRouting> remoteHostStaticRouting =
+        ipv4RoutingHelper.GetStaticRouting(remoteHost->GetObject<Ipv4>());
+    remoteHostStaticRouting->AddNetworkRouteTo(Ipv4Address("7.0.0.0"), Ipv4Mask("255.0.0.0"), 1);
+    internet.Install(ueNodes);
+
+    Ipv4InterfaceContainer ueIpIface;
+    ueIpIface = epcHelper->AssignUeIpv4Address(NetDeviceContainer(ueNetDev));
+
+    // assign IP address to UEs, and install UDP downlink applications
+    uint16_t dlPort = 1234;
+    ApplicationContainer clientApps;
+    ApplicationContainer serverApps;
+    for (uint32_t u = 0; u < ueNodes.GetN(); ++u)
+    {
+        Ptr<Node> ueNode = ueNodes.Get(u);
+        // Set the default gateway for the UE
+        Ptr<Ipv4StaticRouting> ueStaticRouting =
+            ipv4RoutingHelper.GetStaticRouting(ueNode->GetObject<Ipv4>());
+        ueStaticRouting->SetDefaultRoute(epcHelper->GetUeDefaultGatewayAddress(), 1);
+
+        UdpServerHelper dlPacketSinkHelper(dlPort);
+        serverApps.Add(dlPacketSinkHelper.Install(ueNodes.Get(u)));
+
+        UdpClientHelper dlClient(ueIpIface.GetAddress(u), dlPort);
+        dlClient.SetAttribute("Interval", TimeValue(MicroSeconds(1)));
+        // dlClient.SetAttribute ("MaxPackets", UintegerValue(0xFFFFFFFF));
+        dlClient.SetAttribute("MaxPackets", UintegerValue(10));
+        dlClient.SetAttribute("PacketSize", UintegerValue(1500));
+        clientApps.Add(dlClient.Install(remoteHost));
+    }
+
+    // attach UEs to the closest eNB
+    nrHelper->AttachToClosestEnb(ueNetDev, enbNetDev);
+
+    // start server and client apps
+    serverApps.Start(Seconds(0.4));
+    clientApps.Start(Seconds(0.4));
+    serverApps.Stop(Seconds(simTime));
+    clientApps.Stop(Seconds(simTime - 0.2));
+
+    // enable the traces provided by the nr module
+    nrHelper->EnableTraces();
+
+    Simulator::Stop(Seconds(simTime));
+    Simulator::Run();
+
+    Ptr<UdpServer> serverApp = serverApps.Get(0)->GetObject<UdpServer>();
+    uint64_t receivedPackets = serverApp->GetReceived();
+
+    Simulator::Destroy();
+
+    if (receivedPackets == 10)
+    {
+        return EXIT_SUCCESS;
+    }
+    else
+    {
+        return EXIT_FAILURE;
+    }
+}
